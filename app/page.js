9696b75dfb40d4d29623fb9d0443c4d5c87bf7af
Mahesh <maheshkhatri2058@gmail.com>
2025-06-12 23:29:34 +0530

    all updated is pushed

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import projects from "./jsondata/project.json";
 import services from "./jsondata/services.json";
 import teamData from "./jsondata/member.json";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination ,Autoplay} from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -37,16 +37,16 @@ export default function Home() {
          className="bg-[url('/banner.png')] relative min-h-screen max-sm:min-h-[650px]  flex flex-col justify-center items-center text-center px-6 py-20 max-sm:py-2 bg-blue-100 text-black">
           <div className=" absolute inset-0" />
           <div className="relative z-10 sm:py-2">
-          <h1 className="text-4xl md:text-6xl font-bold text-orange-600 mb-4">
+          <h1 className="text-4xl md:text-6xl font-bold text-yellow-500 mb-4">
       {text}
       <span className="animate-pulse">|</span>
     </h1>
             <p className="text-lg md:text-xl text-white max-w-xl mx-auto mb-6">
-              At MKVerse, we transform ideas into powerful digital experiences
-              through stunning websites, intuitive apps, and meaningful design
+              "At MKVerse, we transform ideas into powerful digital experiences
+              through stunning websites, intuitive apps, and meaningful design"
             </p>
             <Link href="#services">
-              <button className="bg-yellow-500 text-black font-semibold px-6 py-3 rounded-full hover:bg-cyan-400 transition">
+              <button className="bg-pink-500 text-black font-semibold px-6 py-3 rounded-full hover:bg-cyan-400 transition">
                 Explore Our Work
               </button>
             </Link>
@@ -55,42 +55,39 @@ export default function Home() {
 
         {/* About Section */}
         <section className="py-10 px-12 text-center bg-white flex gap-12 max-sm:p-2 max-md:flex-col-reverse">
-          <div className="px-8 max-sm:w-full rounded-b-4xl max-md:w-full max-sm:px-2">
-            <div className="p-10 max-sm:p-2 max-md:w-full">
-              <h2 className="text-3xl font-bold text-orange-400 mb-4">
+            <div className="py-25 max-w:1/2 max-sm:p-2 max-md:w-full">
+              <h2 className="text-3xl text-left font-bold text-orange-400 mb-4">
                 What is MKVerse?
               </h2>
-              <p className="text-lg text-black max-w-3xl mx-auto p-2">
-                MKVerse is a digital studio where innovation meets creativity.
-                We specialize in web and app development, UI/UX design, and
-                visual content creation — all tailored to make your digital
-                presence shine.
+              <p className="text-lg text-left text-black max-w-3xl mx-auto p-2">
+               MKVerse is a creative digital studio offering custom web & app development, UI/UX design, and visual content services. We help startups and businesses build modern, user-focused digital experiences that stand out.Let’s turn your ideas into powerful digital products.
               </p>
-              <button className="bg-yellow-500 text-black font-semibold px-6 py-3 rounded-full hover:bg-cyan-400 transition">
+              <div className="flex text-left">
+                <button className="bg-yellow-500 items-left text-black font-semibold px-6 py-3 rounded-full hover:bg-cyan-400 transition">
                 Learn More
               </button>
-            </div>
-          </div>
-          <div className="py-10 px-10 max-sm:px-2">
+              </div>
+            </div>         
+           <div className="w-1/2 max-sm:px-2">
             <Image
-              src="/about.jpg"
-              height={400}
-              width={400}
+              src="/bnr3.jpg"
+              height={500}
+              width={500}
               alt="about pic"
             ></Image>
           </div>
         </section>
 
         {/* Services Section */}
-        <section id="services" className="py-20 px-6 text-center bg-blue-100">
+        <section id="services" className="py-20 px-6 text-center bg-slate-100">
           <h2 className="text-3xl font-bold text-orange-800  mb-10">
             Our Services
           </h2>
-          <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 max-w-7xl mx-auto px-4 py-10">
+          <div className="grid text-black gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 max-w-7xl mx-auto px-4 py-10">
             {services.map(({ title, url, desc }) => (
               <div
                 key={title}
-                className="bg-gray-900 text-white rounded-2xl overflow-hidden shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition-all duration-300"
+                className="bg-white text-white rounded-2xl overflow-hidden shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition-all duration-300"
               >
                 <div className="w-full h-[180px] overflow-hidden p-4">
                   <Image
@@ -103,9 +100,9 @@ export default function Home() {
                 </div>
                 <div className="p-5 space-y-2">
                   <h3 className="text-lg font-bold text-orange-500">{title}</h3>
-                  <p className="text-sm text-gray-300">{desc}</p>
+                  <p className="text-sm text-black">{desc}</p>
                   <button className="bg-blue-400 rounded-md p-1 hover:bg-amber-400">
-                    Book Now
+                    <Link href='/contact'>Lets Connect</Link>
                   </button>
                 </div>
               </div>
@@ -141,16 +138,18 @@ export default function Home() {
             </h2>
 
             <Swiper
-              modules={[Navigation]}
-              spaceBetween={20}
-              slidesPerView={1}
-              navigation
-              pagination={{ clickable: true }}
-              breakpoints={{
-                640: { slidesPerView: 1 },
-                768: { slidesPerView: 2 },
-                1024: { slidesPerView: 3 },
-              }}
+              modules={[ Autoplay]}
+  spaceBetween={20}
+  slidesPerView={1}
+  autoplay={{
+    delay: 2500,  // 2.5 seconds between slides
+    disableOnInteraction: false, // continues autoplay after user interaction
+  }}
+  breakpoints={{
+    640: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  }}
             >
               {teamData.map((member, index) => (
                 <SwiperSlide key={index}>
@@ -172,21 +171,21 @@ export default function Home() {
                     </p>
                     <div className="flex justify-center gap-6 text-gray-600 text-2xl mt-6">
                       <a
-                        href="https://facebook.com/yourpage"
+                        href={member.fburl}
                         target="_blank"
                         rel="noopener noreferrer"
                       >
                         <FaFacebookF className="hover:text-blue-600 transition" />
                       </a>
                       <a
-                        href="https://instagram.com/yourpage"
+                        href={member.instaurl}
                         target="_blank"
                         rel="noopener noreferrer"
                       >
                         <FaInstagram className="hover:text-pink-500 transition" />
                       </a>
                       <a
-                        href="https://linkedin.com/in/yourprofile"
+                        href={member.linkdurl}
                         target="_blank"
                         rel="noopener noreferrer"
                       >
